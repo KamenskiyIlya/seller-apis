e8665df0b01b666032e8b52f71787d04ca66bcad
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-01-08 17:36:44 +0300

    Add docstrings to market.py

--- market.py
+++ market.py
@@ -11,6 +11,24 @@ logger = logging.getLogger(__file__)
 
 
 def get_product_list(page, campaign_id, access_token):
+    """Получает список товаров из магазина на Яндекс маркете.
+
+    Args:
+    page (str): Токен страницы с товарами.
+    campaign_id (str): ID магазина.
+    access_token (str): API токен продавца.
+
+    Returns:
+    list: Список товаров из магазина.
+
+    Examples:
+    >>> get_product_list('1', '1234', 'Ab123CDe45')
+    [...]
+
+    >>> get_product_list(10, 1234)
+    TypeError: price_conversion() missing 1 required
+        positional argument: 'access_token'
+    """    
     endpoint_url = "https://api.partner.market.yandex.ru/"
     headers = {
         "Content-Type": "application/json",
@@ -30,6 +48,26 @@ def get_product_list(page, campaign_id, access_token):
 
 
 def update_stocks(stocks, campaign_id, access_token):
+    """Обновляет информацию по остаткам часов на Ozon.
+
+    Args:
+    stocks(:obj:'list' of :obj:'dict'): Информация об остатках часов
+        продающихся на Яндекс маркете.
+    campaign_id (str): ID магазина.
+    access_token (str): API токен продавца.
+
+    Returns:
+    dict: Cловарь с ответом от Яндекс маркета.
+
+    Examples:
+    >>> update_price(stocks, '1234', 'Ab123CDe45')
+        От API Яндекс маркета вернется словарь, сообщающий об
+        успешном обновлении остатков.
+
+    >>> update_price(10, '1234', 'Ab123CDe45')
+        От API Яндекс маркета вернется словарь, сообщающий
+        об ошибке при обновлении остатков
+    """
     endpoint_url = "https://api.partner.market.yandex.ru/"
     headers = {
         "Content-Type": "application/json",
@@ -46,6 +84,26 @@ def update_stocks(stocks, campaign_id, access_token):
 
 
 def update_price(prices, campaign_id, access_token):
+    """Обновляет цены на товары в магазине Яндекс маркета.
+
+    Args:
+    prices(:obj:'list' of :obj:'dict'): Информация о цене
+        по каждому товару.
+    campaign_id (str): ID магазина.
+    access_token (str): API токен продавца.
+
+    Returns:
+    dict: Cловарь с ответом от API Яндекс маркета.
+
+    Examples:
+    >>> update_price(prices, '1234', 'Ab123CDe45')
+        От API Яндекс маркета вернется словарь, сообщающий
+        об успешной загрузке прайсов.
+
+    >>> update_price(10, '1234', 'Ab123CDe45')
+        От API Яндекс маркета вернется словарь, сообщающий
+        об ошибке при загрузке прайсов.
+    """
     endpoint_url = "https://api.partner.market.yandex.ru/"
     headers = {
         "Content-Type": "application/json",
@@ -62,7 +120,22 @@ def update_price(prices, campaign_id, access_token):
 
 
 def get_offer_ids(campaign_id, market_token):
-    """Получить артикулы товаров Яндекс маркета"""
+    """Получает артикулы товаров из магазина Яндекс маркета.
+
+    Args:
+    campaign_id (str): ID магазина.
+    market_token (str): API токен продавца.
+
+    Returns:
+    list: Список артикулов товаров продающихся в магазине.
+
+    Examples:
+    >>> get_offer_ids('1234', 'Ab123CDe45')
+    [...]
+
+    >>> get_offer_ids(10, 1234)
+        Вернется ошибка от API Ozon.
+    """
     page = ""
     product_list = []
     while True:
@@ -78,7 +151,32 @@ def get_offer_ids(campaign_id, market_token):
 
 
 def create_stocks(watch_remnants, offer_ids, warehouse_id):
-    # Уберем то, что не загружено в market
+    """Формирует список с остатками по часам продающимся в магазине.
+
+    Сначала функция формирует остатки для всех товаров, которые
+    продаются компанией ООО "Группа АВГУСТ" и в магазине Яндекс маркета,
+    после этого удаляя артикул из общего списка артикулов offer_ids.
+    Далее по оставшимся артикулам в списке offer_ids, которые
+    не содержатся в списке часов с сайта ООО "Группа АВГУСТ",
+    формируются нулевые остатки.
+
+    Args:
+    watch_remnants (dict): Cловарь с данными о часах,
+        продающихся на сайте компании ООО "Группа АВГУСТ".
+    offer_ids (list): Список артикулов часов продающихся на Ozon.
+    warehouse_id (str): ID склада, на котором хранится товар.
+
+    Returns:
+    :obj:'list' of :obj:'dict': Список словарей содержащих артикула
+        товаров и их остатки на складе.
+
+    Examples:
+    >>> create_stocks(watch_remnants, offer_ids)
+    [{...},{...},...]
+
+    >>> create_stocks('часы', 10, 10)
+    AttributeError: 'str' object has no attribute 'get'
+    """
     stocks = list()
     date = str(datetime.datetime.utcnow().replace(microsecond=0).isoformat() + "Z")
     for watch in watch_remnants:
@@ -123,6 +221,24 @@ def create_stocks(watch_remnants, offer_ids, warehouse_id):
 
 
 def create_prices(watch_remnants, offer_ids):
+    """Формирует цены для часов, которые продаются в магазине Ozon.
+
+    Args:
+    watch_remnants (dict): Словарь с данными о часах,
+        продающихся на сайте компании ООО "Группа АВГУСТ".
+    offer_ids (list): Список артикулов часов продающихся на Ozon.
+
+    Returns:
+    :obj:'list' of :obj:'dict': Список словарей содержащих информацию
+        об актуальных ценах часов продающихся в магазине Яндекс маркета.
+
+    Examples:
+    >>> create_prices(watch_remnants, offer_ids)
+    [{...},{...},...]
+
+    >>> create_prices('часы', 10)
+    AttributeError: 'str' object has no attribute 'get'
+    """
     prices = []
     for watch in watch_remnants:
         if str(watch.get("Код")) in offer_ids:
@@ -143,6 +259,25 @@ def create_prices(watch_remnants, offer_ids):
 
 
 async def upload_prices(watch_remnants, campaign_id, market_token):
+    """Асинхронно обновляет цены на товары продающиеся на Яндекс маркете.
+
+    Args:
+    watch_remnants (dict): Словарь с данными о часах,
+        продающихся на сайте компании ООО "Группа АВГУСТ".
+    campaign_id (str): ID магазина.
+    market_token (str): API токен продавца.
+
+    Returns:
+    :obj:'list' of :obj:'dict': Возвращает прайсы, которые были подгружены
+        на сайт Яндекс маркета.
+
+    Examples:
+    >>> create_prices(watch_remnants, client_id, seller_token)
+    [{...},{...},...]
+
+    >>> create_prices('часы', '1234', 'Ab123CDe45')
+    AttributeError: 'str' object has no attribute 'get'
+    """
     offer_ids = get_offer_ids(campaign_id, market_token)
     prices = create_prices(watch_remnants, offer_ids)
     for some_prices in list(divide(prices, 500)):
@@ -151,6 +286,28 @@ async def upload_prices(watch_remnants, campaign_id, market_token):
 
 
 async def upload_stocks(watch_remnants, campaign_id, market_token, warehouse_id):
+    """Асинхронно обновляет остатки товаров продающихся на Яндекс маркете.
+
+    Args:
+    watch_remnants (dict): Словарь с данными о часах,
+        продающихся на сайте компании ООО "Группа АВГУСТ".
+    campaign_id (str): ID магазина.
+    market_token (str): API токен продавца.
+    warehouse_id (str): ID склада, на котором хранится товар.
+
+    Returns:
+    :obj:'list' of :obj:'dict': Список с информацией об остатках товаров
+        с ненулевыми остатками на складе.
+    :obj:'list' of :obj:'dict': Список с информацией об остатках
+        всех товаров на складе.
+
+    Examples:
+    >>> upload_stocks(watch_remnants, client_id, seller_token)
+    [{...},{...},...], [{...},{...},...]
+
+    >>> upload_stocks('часы', '1234', 'Ab123CDe45')
+    AttributeError: 'str' object has no attribute 'get'
+    """
     offer_ids = get_offer_ids(campaign_id, market_token)
     stocks = create_stocks(watch_remnants, offer_ids, warehouse_id)
     for some_stock in list(divide(stocks, 2000)):
